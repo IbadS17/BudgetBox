8b4f30fb9eef1bd427173cf85650bede33fba793
Mohd Ibaad <ibads66585@gmail.com>
2025-11-23 00:38:18 +0530

    Fix: Service-Worker for login

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -8,14 +8,20 @@ self.addEventListener("activate", () => {
 });
 
 self.addEventListener("fetch", (event) => {
+  // Ignore non-GET requests
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.open("budgetbox-v1").then((cache) => {
-      return fetch(event.request)
-        .then((response) => {
-          cache.put(event.request, response.clone());
-          return response;
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request).then((networkResponse) => {
+          return caches.open("v1").then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
         })
-        .catch(() => cache.match(event.request));
+      );
     })
   );
 });
